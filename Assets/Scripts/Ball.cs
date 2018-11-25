b777679e0ab8b74de03f3e7eae5caa152d91a913
Marcus Piazzola <mpiazzola@Marcuss-MBP.fios-router.home>
2018-11-25 15:16:28 -0500

    Updated boundaries and able to launch ball from paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float initXvelocity = 2f;
+    [SerializeField] float initYvelocity = 15f;
+
+    Vector2 paddleToBallVector;
+    bool hasStarted;
+
+	// Use this for initialization
+	void Start () {
+
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        hasStarted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle() {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick() {
+        if (Input.GetMouseButtonDown(0)) {
+
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(initXvelocity, initYvelocity);
+        }
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    [SerializeField] float screenWidthConversion = 16f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,11 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(Input.mousePosition);
+
+        float mouseXpos = Input.mousePosition.x / Screen.width * screenWidthConversion;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXpos, minX, maxX);
+        transform.position = paddlePos;
+        //transform.position = new Vector2(Input.mousePosition.x / Screen.width * screenWidthConversion, transform.position.y);
 	}
 }
